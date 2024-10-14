2e7afa01745b41d0bde67d86b45e973ba9655a5e
Will Dingle <86018656+willdingle@users.noreply.github.com>
2024-10-14 10:27:28 +0100

    C# and F# programs working together in VS solution

---
+++ ExpressionInterpreter/Program.cs
@@ -0,0 +1,27 @@
+﻿using FsharpLib;
+using System;
+
+namespace CSharApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simple Interpreter");
+            var varTable = new Dictionary<string,double>();
+
+            while (true)
+            {
+                Console.Write("-> ");
+                string input = Console.ReadLine();
+
+                var oList = Interpreter.lexer(input);
+                var sList = Interpreter.printTList(oList);
+                //var pList = Interpreter.printTList(Interpreter.parser(oList));
+                var Out = Interpreter.parseNeval(oList, varTable);
+                Console.WriteLine("Result = {0}", Out.Item2);
+            }
+        }
+    }
+}
+
